8d23f65606f62e4ce6520cc7410359d546aac462
H. Joe Lee <hyoklee@hdfgroup.org>
2014-09-05 16:32:39 -0500

    Added source distribution build and installer test.

--- run.bat
+++ run.bat
@@ -2,5 +2,8 @@ set INCLUDE_DIRS=C:\Program Files (x86)\HDF_Group\HDF\4.2.10\include
 set LIBRARY_DIRS=C:\Program Files (x86)\HDF_Group\HDF\4.2.10\lib;C:\Program Files (x86)\HDF_Group\HDF\4.2.10\bin
 python setup.py build
 python setup.py bdist_msi
+python setup.py sdist --formats=gztar,zip
+dist\pyhdf-0.9.0.win32-py2.7.msi
+
 
 
